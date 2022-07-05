050aa0d1e2d50de88c0535608edac4a3a0818bd0
Jefferyjasmin <71089889+Jefferyjasmin@users.noreply.github.com>
2022-07-05 18:08:03 -0400

    landingpage updated

--- src/Banner.js
+++ src/Banner.js
@@ -8,53 +8,65 @@ const Banner = () => {
       <div className="dark-overlay landing-inner ">
         <div className="containers">
           <div className="left">
-            <h2 style={{ color: "white" }}> Software Engineer</h2>
-            <h3 className="left_content">
-              I have a passion for engineering and tackling new projects . I
-              love solving real world problems, while bringing ideas to UI
-            </h3>
+            <div className="banner_content">
+              <h2 style={{ color: "white" }}> Software Engineer</h2>
+              <h3 className="left_content">
+                Hi, My name is jeffery jasmin
+                <p style={{ marginTop: "20px", fontSize: "1rem" }}>
+                  I create interfaces and experiences, love solving real world
+                  problems, while bringing ideas to UI
+                </p>
+              </h3>{" "}
+              <span>Front End Develpoment</span> /
+              <span>Back End Develpoment</span> /
+              <span>FullStack Develpoment</span>
+              <div className="right">
+                <button className="btn btn-primary profiles">
+                  <a
+                    href="/work"
+                    style={{ textDecoration: "none" }}
+                    target="_blank"
+                  >
+                    {" "}
+                    Profile
+                  </a>
+                </button>
+                <button className="Resume btn btn-primary ">
+                  <a
+                    href="https://drive.google.com/file/d/16YHVmxvZxFOCmKkZ9LEVhvonZ8Jsgtn2/view?usp=sharing"
+                    target="_blank"
+                  >
+                    {" "}
+                    Resume
+                  </a>
+                </button>
+
+                <button className="btn btn-primary Github">
+                  <a
+                    href="https://github.com/Jefferyjasmin"
+                    style={{ textDecoration: "none" }}
+                    target="_blank"
+                  >
+                    Github
+                  </a>
+                </button>
+                <button className="btn btn-primary LinkedIn">
+                  <a
+                    href="https://www.linkedin.com/in/jefferyjasmin/"
+                    style={{ textDecoration: "none", color: "white" }}
+                    target="_blank"
+                  >
+                    {" "}
+                    LinkedIn
+                  </a>
+                </button>
+              </div>
+            </div>
+
             {/* <button style={{ height: "10%", width: "30%" }}>MY_Profile</button> */}
           </div>
-          <div className="right">
-            <button className="Resume btn btn-info ">
-              <a
-                href="https://drive.google.com/file/d/16YHVmxvZxFOCmKkZ9LEVhvonZ8Jsgtn2/view?usp=sharing"
-                target="_blank"
-              >
-                {" "}
-                Resume
-              </a>
-            </button>
-
-            <button className="btn btn-primary profiles">
-              <a
-                href="/work"
-                style={{ textDecoration: "none" }}
-                target="_blank"
-              >
-                {" "}
-                Profile
-              </a>
-            </button>
-            <button className="btn btn-secondary Github">
-              <a
-                href="https://github.com/Jefferyjasmin"
-                style={{ textDecoration: "none" }}
-                target="_blank"
-              >
-                Github
-              </a>
-            </button>
-            <button className="btn btn-info LinkedIn">
-              <a
-                href="https://www.linkedin.com/in/jefferyjasmin/"
-                style={{ textDecoration: "none", color: "white" }}
-                target="_blank"
-              >
-                {" "}
-                LinkedIn
-              </a>
-            </button>
+          <div className="banner_image">
+            <h1>J</h1>
           </div>
         </div>
       </div>
