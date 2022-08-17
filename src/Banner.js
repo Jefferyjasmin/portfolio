ecf1387b73c81a4e55c3f5a63221087e867107d8
Jefferyjasmin <71089889+Jefferyjasmin@users.noreply.github.com>
2022-08-17 14:25:48 -0400

    banner update

--- src/Banner.js
+++ src/Banner.js
@@ -15,37 +15,37 @@ const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const period = 2000;
 
-  // useEffect(() => {
-  //   let ticker = setInterval(() => {
-  //     tick();
-  //   }, delta);
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-  //   return () => {
-  //     clearInterval(ticker);
-  //   };
-  // }, [text]);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
-  // const tick = () => {
-  //   let i = loopNum % toRotate.length;
-  //   let fullText = toRotate[i];
-  //   let updatedText = isDeleting
-  //     ? fullText.substring(0, text.length - 1)
-  //     : fullText.substring(0, text.length + 1);
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
-  //   setText(updatedText);
+    setText(updatedText);
 
-  //   if (isDeleting) {
-  //     setDelta((prevDelta) => prevDelta / 2);
-  //   }
-  //   if (!isDeleting && updatedText === fullText) {
-  //     setIsDeleting(true);
-  //     setDelta(period);
-  //   } else if (isDeleting && updatedText === "") {
-  //     setIsDeleting(false);
-  //     setLoopNum(loopNum + 1);
-  //     setDelta(500);
-  //   }
-  // };
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
 
   return (
     <div className="banner">
